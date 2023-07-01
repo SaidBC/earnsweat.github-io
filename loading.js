e1a581c4c343e2747451ab1c402c1bb6aa05d5dd
SaidBC <45534225+SaidBC@users.noreply.github.com>
2023-07-01 14:15:42 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,10 +1,16 @@
-        var loaded = window.sessionStorage.getItem("loaded")
+        var loaded = window.sessionStorage.getItem("loaded"),loaded2 = window.sessionStorage.getItem("loaded2");
         window.addEventListener("load",()=>{
          var header = document.querySelector("header"),form = document.querySelector("form"),div = document.querySelector("div")
-         if(loaded != "false"){
+         if(loaded != "false" && window.location.href == "https://saidbc.github.io/earnsweat.github-io/login.html"){
              form.style.animation="toRight 1s ease-in-out 1"
              div.style.animation="toRight 1.5s ease-in-out 1"
              header.style.animation="toTop 1s ease-out 1"
              window.sessionStorage.setItem("loaded",false)
+         }
+         if(loaded2 != "false" && window.location.href == "'https://saidbc.github.io/earnsweat.github-io/register.html'"){
+             form.style.animation="toRight 1s ease-in-out 1"
+             div.style.animation="toRight 1.5s ease-in-out 1"
+             header.style.animation="toTop 1s ease-out 1"
+             window.sessionStorage.setItem("loaded2",false)
          }
      })
