386ca1d80a1c9172127a269a2ba6d6ca12ce0c92
SaidBC <45534225+SaidBC@users.noreply.github.com>
2023-07-01 14:26:26 +0000

    Update loading.js

--- loading.js
+++ loading.js
@@ -6,7 +6,7 @@
              div.style.animation="toRight 1.5s ease-in-out 1"
              header.style.animation="toTop 1s ease-out 1"
              window.sessionStorage.setItem("loaded",false)
-         }else if(loaded2 != "false" && window.location.href == "'https://saidbc.github.io/earnsweat.github-io/register.html'"){
+         }else if(loaded2 != "false" && window.location.href == "https://saidbc.github.io/earnsweat.github-io/register.html"){
              form.style.animation="toRight 1s ease-in-out 1"
              div.style.animation="toRight 1.5s ease-in-out 1"
              header.style.animation="toTop 1s ease-out 1"
